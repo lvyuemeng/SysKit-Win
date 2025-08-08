af37bb340c9ee53b40fd1f96287a868b879b229c
lvyuemeng <lv15623448685@gmail.com>
2025-08-08 19:50:19 +0800

    feat: reduce and redesign the project; add scoop-setup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## SysKit
 
 A Basic System Kit for Windows System initiation and setup.
+
+## ⚖️ License
+
+[Apache 2.0 License](/LICENSE-Apache) Or [MIT License](/LICENSE-MIT) - Copyright(C)
---
+++ scoop-setup.ps1
@@ -0,0 +1,62 @@
+param (
+	[CmdletBinding()]
+	[ValidateSet("china", "proxy", "native")]
+	[string]$req = "proxy",
+	[Parameter(ValueFromRemainingArguments = $true)]
+	[string[]]$args
+)
+
+# --- Help ---
+
+$help_ctx = """
+This script will install scoop.
+
+Usage: scoop-setup.ps1 [options]
+
+Options:
+	-req [native|china|proxy] default: proxy
+"""
+
+$may_help = $args | Where-Object {
+	$_ -match "-(h|/?)|--help"
+}
+
+if ($may_help) {
+	Write-Host $help_ctx
+	return
+}
+
+# --- Prepare ---
+
+$exp = switch ($req) {
+	"china" {
+		"Invoke-WebRequest -useb scoop.201704.xyz | Invoke-Expression"
+	}
+	"proxy" {
+		"Invoke-WebRequest https://ghfast.top/raw.githubusercontent.com/lzwme/scoop-proxy-cn/main/install.ps1 | Invoke-Expression"
+	}
+	"native" {
+		"Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression"
+	}
+}
+
+# --- Install ---
+
+Write-Host "Installing scoop..."
+if (Get-Command "scoop" -ErrorAction SilentlyContinue) {
+	Write-Host "scoop already installed, updating..."
+	& scoop update
+	return
+}
+
+# install scoop
+Invoke-Expression $exp
+
+# config repo/bucket
+& scoop config SCOOP_REPO "https://gitee.com/scoop-installer/scoop"
+& scoop bucket add spc "https://gitee.com/wlzwme/scoop-proxy-cn.git"
+
+# update bucket spc
+& Set-Location "$env:USERPROFILE\scoop\buckets\spc"
+& git fetch -all && git checkout -b main origin/main
+
